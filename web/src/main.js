d06e155aaf94c678c03a8c73364de548eb488159
LeenSamZiv <1179364851@qq.com>
2019-06-12 10:00:44 +0800

    ✔ close vue-dev-tool  with production environment

--- web/src/main.js
+++ web/src/main.js
@@ -3,6 +3,12 @@ import App from './App.vue'
 import SocketServer from "./util/SocketServer";
 import store from './store'
 
+//关闭vue-dev-tool
+const isDebug_mode = process.env.NODE_ENV !== 'production';
+Vue.config.debug = isDebug_mode;
+Vue.config.devtools = isDebug_mode;
+Vue.config.productionTip = isDebug_mode;
+
 Vue.config.productionTip = false;
 Vue.prototype.$socketServer = new SocketServer();
 
